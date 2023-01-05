515694cad56b2ca0a337ffa8356caef1b7574300
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-01-04 20:08:06 -0500

    Create JS test.js

---
+++ JS test.js
@@ -0,0 +1,33 @@
+// change the value of `education` to test your code
+var education = 'no high school diploma';
+
+// set the value of this based on a person's education
+var salary = 0;
+
+// your code goes here
+
+switch (education) {
+    case "no high school diploma":
+        salary = 25636;
+        break;
+    case "a high school diploma":
+        salary = 35256;
+        break;
+    case "an Associate's degree":
+        salary = 41496;
+        break;
+    case "a Bachelor's degree":
+        salary = 59124;
+        break;
+    case "a Master's degree":
+        salary = 69732;
+        break;
+    case "a Professional degree":
+        salary = 89960;
+    case "a Doctoral degree":
+        salary = 84396;
+    default:
+        console.log "Please enter a valid educational background.";
+}
+
+console.log ("In 2015, a person with " +education+ " earned an average of $ " +salary.toLocaleString("en-US")+"/year.");
