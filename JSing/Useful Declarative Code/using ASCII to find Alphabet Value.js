7047ba1474a17afa56e49ca5bac253d9f5d90771
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-01-24 18:07:17 -0500

    More CodeWar nonsence.

---
+++ JSing/Useful Declarative Code/using ASCII to find Alphabet Value.js
@@ -0,0 +1,19 @@
+function alphabetPosition(text) {
+    var strNum = "";
+    
+    
+    for (var i = 0; i < text.length; i++){
+      
+      //conversion of text to ascii  
+      var code = text.toUpperCase().charCodeAt(i)
+      //ensuring to check for only letters
+      // code - 64 can be the magic number to designate 1-26 for A-Z.
+      if (code > 64 && code < 91) strNum += (code - 64) + " ";
+    }
+    
+    //ensuring the output is trimmed with no spaces on the end.
+    let result = strNum.slice(0, strNum.length-1);
+    console.log(result);
+  }
+  
+   console.log(alphabetPosition('abcde'));
