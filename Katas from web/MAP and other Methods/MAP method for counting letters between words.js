8192378a52295c18cf441545af14f4bb051f4b28
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-02-04 16:22:38 -0500

    Practice

--- Katas from web/MAP and other Methods/MAP method for counting letters between words.js
+++ Katas from web/MAP and other Methods/MAP method for counting letters between words.js
@@ -1,9 +1,15 @@
 function test(str) {
-    return str.split(' ')
-    .map(word => {
-        return (word.length > 4) 
-        ? `${word[0]}${word.length-2}${word[word.length-1]}` : word 
-    })
-    .join(' ');
-    }
     
+    // create the mini function first to simplify the MAP method later.
+    const trim = (word) => 
+    `${word[0]}${word.length-2}${word[word.length-1]}`;
+    
+    // beginning of the Array method.
+    return str
+    .split(" ")
+    // arrow function with ternary operator.
+    .map(words => (words.length > 4 ? trim(words) : words))
+    .join(" ");
+}
+
+console.log(test("Every developer likes to mix kubernetes and javascript"));
