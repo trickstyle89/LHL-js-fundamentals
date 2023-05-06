cf2ad4b6c85dee5e0118df4abc414bbf017b539b
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-05-06 07:05:58 -0300

    converting to TS

--- JS Basics/Simple Code Exercises/AreaCal.ts
+++ JS Basics/Simple Code Exercises/AreaCal.ts
@@ -1,16 +1,16 @@
-function calculateRectangleArea(length, width) {
+function calculateRectangleArea(length: number, width: number) {
     if (length > 0 && width > 0) {
       return width * length;
      }
   }
   
-  function calculateTriangleArea(base, height) {
+  function calculateTriangleArea(base: number, height: number) {
       if (base > 0 && height > 0) {
           return base * height / 2;
      }
   }
   
-  function calculateCircleArea(radius) {
+  function calculateCircleArea(radius: number) {
       if (radius > 0) {
           return Math.PI * (radius * radius);
       }
